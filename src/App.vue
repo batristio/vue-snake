871008fb166b890d43f0801e9ca9c1c071f4a4e0
Chris Krastevski <chris.krastevski@qualsafeawards.org>
2020-05-15 23:28:17 +0100

    WIP: * Field for the game is generated * Initial x, y position of the snake as well, * |ToDo|: testing and need to try tdd

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,48 @@
 <template>
   <div id="app">
-    <span>Empty app</span>
+    <div
+      v-for="(entry, index) in matrix"
+      :key="index"
+    >
+      <span>{{ entry }}</span> {{ index }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Vue } from 'vue-property-decorator'
+import { random } from './utils/index'
 
-@Component({
-  //
+export default Vue.extend({
+  data () {
+    return {
+      matrix: [],
+      direction: 'right',
+      position: ''
+    }
+  },
+  methods: {
+    generateMatrix (size: number): void {
+      if (size < 20) throw new Error('Size should be at least 20 x 20.')
+
+      for (let i = 0; i < size; i++) {
+        this.$data.matrix[i] = this.getFilledArrayWithZeros(size)
+      }
+
+      this.generateInitialSnakePosition('', size)
+    },
+    generateInitialSnakePosition (direction: string, size: number): void {
+      const x = random(2, size - 2)
+      const y = random(2, size - 2)
+      this.$data.position = `${x}:${y}`
+      this.$data.matrix[x][y] = 1
+    },
+    getFilledArrayWithZeros (size: number): number[] {
+      return new Array(size).fill(0, 0, size)
+    }
+  },
+  beforeMount () {
+    this.generateMatrix(20)
+  }
 })
-export default class App extends Vue {}
 </script>
---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+const random = function number (min: number, max: number):number {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+export {
+  random
+}
